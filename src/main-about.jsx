7d0a82514ede60bbf0d3b71515243c728e08d1bc
Christopher Owens <cowensdev@gmail.com>
2025-06-11 19:11:03 -0400

    Fix style import

--- src/main-about.jsx
+++ src/main-about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import About from "./pages/About";
-import "./styles.css";
+import "../styles.css";
 import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
